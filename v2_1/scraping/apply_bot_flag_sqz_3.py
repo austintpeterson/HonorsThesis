26495ca53e91b6696a30cee00e56e842d1ac139e
Austin Peterson <austintpeterson@gmail.com>
2019-02-27 17:47:16 -0700

    added bot flag applier for the _sqz workflow

---
+++ v2_1/scraping/apply_bot_flag_sqz_3.py
@@ -0,0 +1,83 @@
+#3
+#(applies classifications)
+#this script applies classifications based on top/bottom 20% (1 or 0)
+#and the 20-30% as t1, and 70-80% as t2
+#written to determine mis-classifications in the upper/lower margins
+
+import csv
+
+import os
+import os.path
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+def main():
+	file_str = input("enter file to apply flags to (_r incl.): ")
+	file_str_ext = file_str+".csv"
+	# thresh = float(input("enter a confidence threshold for classification: "))
+	#thresh
+
+
+
+	#pathways
+	my_path = os.path.dirname(os.path.abspath(__file__))
+	parent_path = os.path.abspath(os.path.join(my_path, os.pardir))
+
+
+
+	with open(parent_path+"/user_collections/"+file_str_ext, "r") as file:
+		reader = csv.reader(file, delimiter = ',')
+		next(reader, None)#skip header
+
+		#writer
+		#newfile = open(parent_path+"/user_collections/"+file_str+"_"+str(thresh)+".csv", "w")
+		newfile = open(parent_path+"/user_collections/"+file_str+"_sqz"+".csv", "w")
+
+		writer = csv.writer(newfile, delimiter = ',')
+		writer.writerow(['id', 'screen_name', 'name', 'date_created', 'botornot_rt', 'bot'])
+
+		i = 0
+		for row in reader:
+
+			#determine flag based on thresh
+			print(str(i)+": classifying @"+row[1]+"...")
+			bot_rt = float(row[4])
+
+			#botornot_rt exceeds user def. threshold,
+			#classified as a bot (1)
+			"""
+			if bot_rt >= thresh:
+				writer.writerow([str(row[0]), row[1], row[2], row[3], row[4], "1"])
+			else:
+				writer.writerow([str(row[0]), row[1], row[2], row[3], row[4], "0"])
+			"""
+
+			if bot_rt >= .80:
+				#upper training (bots)
+				writer.writerow([str(row[0]), row[1], row[2], row[3], row[4], "1"])
+			elif bot_rt <= .20:
+				#lower training (nots)
+				writer.writerow([str(row[0]), row[1], row[2], row[3], row[4], "0"])
+			elif .20 < bot_rt <= .30:
+				#lower test
+				writer.writerow([str(row[0]), row[1], row[2], row[3], row[4], "t1"])
+			elif .70 <= bot_rt < .80:
+				#upper test
+				writer.writerow([str(row[0]), row[1], row[2], row[3], row[4], "t2"])
+			else:
+				#middle section
+				writer.writerow([str(row[0]), row[1], row[2], row[3], row[4], "m"])
+
+
+			i += 1
+
+
+
+
+
+main()
+
+
+
+
+ 
